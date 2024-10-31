6692694fbb191063cee8bf56d4cdc3449bd82628
Mheyrie <omota4fidelity@gmail.com>
2024-10-31 03:11:39 +0100

    updated navlink

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from "@/const";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,7 +8,9 @@ const Navbar = () => {
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
         <ul className="hidden h-full gap-12 lg:flex">
-            
+            {NAV_LINKS.map((link,key)=>(
+                <li key={key}>{link.label}</li>
+            ))}
         </ul>
       </Link>
     </nav>
