d2106598eb3ed4659831e0adf23f02bc6514f98b
Mheyrie <omota4fidelity@gmail.com>
2024-10-31 04:02:51 +0100

    Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,6 +28,7 @@ const Navbar = () => {
         variant='btn_dark_green'
         />
       </div>
+      <Image src='/menu.svg 'alt="menu" width={32} height={32} className="" />
     </nav>
   );
 };
