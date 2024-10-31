2fa8a30b24b2a551c0295ecd6ccecdaf6cd6ab60
Mheyrie <omota4fidelity@gmail.com>
2024-10-31 12:46:36 +0100

    Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,39 +5,53 @@ import Button from "./Button";
 import { useState } from "react";
 
 const Navbar = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const toggleHamburger = () =>{
-        setIsSidebarOpen(!isSidebarOpen)
-    }
+  const toggleHamburger = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-      </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
-            {link.label}
+    <>
+        <nav className="flexBetween max-container padding-container relative z-30 py-5">
+          <Link href="/">
+            <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
           </Link>
-        ))}
-      </ul>
-      <div className="lg:flexCenter hidden">
-        <Button 
-        type='button'
-        title='Login'
-        icon='/user.svg'
-        variant='btn_dark_green'
-        />
-      </div>
-
-      {/* Hamburger  */}
-      <Image src='/menu.svg' alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" />
-    </nav>
+          <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+          <div className="lg:flexCenter hidden">
+            <Button
+              type="button"
+              title="Login"
+              icon="/user.svg"
+              variant="btn_dark_green"
+            />
+          </div>
+        
+          {/* Hamburger  */}
+          <button
+            className={`inline-block hamburger md:hidden ${
+              isSidebarOpen ? "open" : ""
+            }`}
+            onClick={toggleHamburger}
+          >
+            <span className="top-ham"></span>
+            <span className="middle-ham"></span>
+            <span className="bottom-ham"></span>
+          </button>
+        </nav>
+        {isSidebarOpen ? (
+            <div className="md:hidden"></div>
+        ):("")}
+    </>
   );
 };
 export default Navbar;
