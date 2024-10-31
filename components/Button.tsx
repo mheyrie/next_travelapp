a0da9b36aa1532abef041086ef01516fa753905d
Mheyrie <omota4fidelity@gmail.com>
2024-10-31 03:28:24 +0100

    Updated

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,10 @@
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
-export default Button
+type ButtonProps = {
+  type: " button" | " submit";
+  title: string;
+  icon?: string;
+  variant: 'btn_dark_green'
+};
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return <button type="button">Button</button>;
+};
+export default Button;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,12 @@ const Navbar = () => {
         ))}
       </ul>
       <div className="lg:flexCenter hidden">
-        <Button/>
+        <Button 
+        type='button'
+        title='Login'
+        icon='/user.svg'
+        variant='btn_dark_green'
+        />
       </div>
     </nav>
   );
