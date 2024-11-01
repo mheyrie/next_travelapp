16b0b2b602e7ea11c78ab67fa9f765f9f738c63d
Mheyrie <omota4fidelity@gmail.com>
2024-11-01 23:20:48 +0100

    Updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image"
+
 export const Hero = () => {
   return (
     <section className="border-2 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
         <div className="hero-map" />
+        {/* Left  */}
+
+        <div className="relative z-20 flex flex-col flex-1 xl:w-1/2">
+        <Image src='/camp.svg' alt="camp" width={50} height={50} />
+        </div>
     </section>
   )
 }
