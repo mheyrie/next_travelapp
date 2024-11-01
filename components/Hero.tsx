f792f76d280c5e9651a981dafbeed8bbe57706a2
Mheyrie <omota4fidelity@gmail.com>
2024-11-01 23:10:36 +0100

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
 export const Hero = () => {
   return (
-    <section className="border-2 max-container padding-container flex flex-col gap-20 py-10 pb-32 ">Hero</section>
+    <section className="border-2 max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+        <div className="hero-map" />
+    </section>
   )
 }
