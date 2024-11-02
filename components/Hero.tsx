de746fac7d528b3f595bf561647d1219472c981c
Mheyrie <omota4fidelity@gmail.com>
2024-11-02 21:04:03 +0100

    Updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,6 +18,8 @@ export const Hero = () => {
           Welcome to <em className="text-orange-700">Mhey</em>
           <em className="text-neutral-500">rie</em> Camping Area
         </h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat dolorum magnam ab eaque? Eius sint quisquam tempore. Autem necessitatibus perspiciatis nobis, cumque sit molestiae! Qui tempora non delectus necessitatibus placeat!
+        Tempore, non nobis voluptatum cumque fugiat perspiciatis quae animi! Laborum dolor distinctio tempora eaque, a excepturi fugit maxime! Suscipit facilis repellendus nulla rerum voluptatem cum dolore officia odit animi esse?</p>
       </div>
     </section>
   );
