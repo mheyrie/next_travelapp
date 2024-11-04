14ba125566f5cd114c17a699d5b7bb78cc33ba52
Mheyrie <omota4fidelity@gmail.com>
2024-11-04 16:00:01 +0100

    CampSite props defined

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,6 +1,13 @@
-const CampSite = () => {
+interface CampProps  {
+  subtitle: string;
+   backgroundImage: string;
+   peopleJoined: string;
+   title: string;
+}
+
+const CampSite = ({subtitle, backgroundImage, peopleJoined, title}:(CampProps)) => {
   return(
-    <div className=""></div>
+    <div className="">Camp Site 1</div>
   )
 }
 
@@ -8,7 +15,14 @@ export default function Camp() {
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 border border-green-600">
       <div className="hide-scrollbar flex w-full items-start h-[340px] justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-        
+        <CampSite 
+        backgroundImage='bg-bg-img-1'
+        title='Pamhaa Resort Corner'
+        subtitle='Prigen, Pasuran'
+        peopleJoined = '50+ Joined'
+        >
+
+        </CampSite>
       </div>
     </section>
   )
