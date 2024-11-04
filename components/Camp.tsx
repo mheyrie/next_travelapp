8ec322753601185118adc581e8eaf9c25c315424
Mheyrie <omota4fidelity@gmail.com>
2024-11-04 15:52:15 +0100

    Added CampSite

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,5 +1,15 @@
+const CampSite = () => {
+  return(
+    <div className=""></div>
+  )
+}
+
 export default function Camp() {
   return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 border border-green-600">Camp</section>
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 border border-green-600">
+      <div className="hide-scrollbar flex w-full items-start h-[340px] justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        
+      </div>
+    </section>
   )
 }
