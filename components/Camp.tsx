c8938ef85b231cf36a72799b94302b4e559321de
Mheyrie <omota4fidelity@gmail.com>
2024-11-06 11:24:09 +0100

    Updated

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -15,7 +15,9 @@ const CampSite = ({
   title,
 }: CampProps) => {
   return (
-    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
           <div className="rounded-full bg-green-50 p-4">
@@ -65,7 +67,22 @@ export default function Camp() {
       </div>
       <div className="flexEnd mt-10 px-6 lg:mt-60 lg:mr-6">
         <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
-          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"></h2>
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+            <strong>Feeling Lost</strong> And Not Knowing the way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            facere quaerat illo possimus voluptatem magnam suscipit accusamus
+            aspernatur rem dolores! Qui accusantium reiciendis deserunt ullam
+            dolor, tempore dignissimos perspiciatis exercitationem.
+          </p>
+          <Image
+            className="camp-quote"
+            src="/quote.svg"
+            alt="camp-2"
+            width={50}
+            height={50}
+          />
         </div>
       </div>
     </section>
