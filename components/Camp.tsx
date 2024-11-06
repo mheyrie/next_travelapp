cba3194ba35669e1cfc2418a5466fa1c3e596e95
Mheyrie <omota4fidelity@gmail.com>
2024-11-06 09:49:34 +0100

    Updated

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -63,7 +63,11 @@ export default function Camp() {
           peopleJoined="50+ Joined"
         />{" "}
       </div>
-      <div className="flexEnd mt-10 px-6 lg:mt-60 lg:mr-6"></div>
+      <div className="flexEnd mt-10 px-6 lg:mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"></h2>
+        </div>
+      </div>
     </section>
   );
 }
