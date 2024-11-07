3be890c7df9a7cda31a3fe58845852e6630ca597
Mheyrie <omota4fidelity@gmail.com>
2024-11-07 19:43:31 +0100

    Updated

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -6,11 +6,15 @@ export default function Guide() {
       <div className="border-1 border-red-600 padding-container max-container w-full p-24">
       <Image
             className="camp-quote"
-            src="/quote.svg"
+            src="/camp.svg"
             alt="camp-2"
             width={50}
             height={50}
           />
+          <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          </p>
+          <div className="flex flex-wrap"></div>
       </div>
     </section>
   )
