45cde035967ea04f93ef6cbfd0209ccd93cbac61
Mheyrie <omota4fidelity@gmail.com>
2024-11-07 19:39:37 +0100

    Guide Updated

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,5 +1,7 @@
 export default function Guide() {
   return (
-    <section className="">Guide</section>
+    <section className="flexCenter flex-col">
+      <div className="border-1 border-red-600 padding-container max-container"></div>
+    </section>
   )
 }
