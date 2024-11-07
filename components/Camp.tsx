ae4f6a24aedd4079344843290ee24134ec5c17ee
Mheyrie <omota4fidelity@gmail.com>
2024-11-07 19:37:46 +0100

    Updated

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -65,7 +65,7 @@ export default function Camp() {
           peopleJoined="50+ Joined"
         />{" "}
       </div>
-      <div className="flexEnd mt-10 px-6 lg:mt-60 lg:mr-6">
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
         <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
           <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
             <strong>Feeling Lost</strong> And Not Knowing the way?
