f9198e53057cae527fc0e62b4cdf0255f539febb
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 13:39:09 +0100

    Updating

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,41 +1,51 @@
-import { FOOTER_LINKS } from "@/const"
-import Image from "next/image"
-import Link from "next/link"
+import { FOOTER_LINKS } from "@/const";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link href='/' className="mb-10">
-          <Image
-          className=""
-          src="hilink-logo.svg"
-          alt="boat"
-          width={74}
-          height={29}
-        />
+          <Link href="/" className="mb-10">
+            <Image
+              className=""
+              src="hilink-logo.svg"
+              alt="boat"
+              width={74}
+              height={29}
+            />
           </Link>
-          <div className="flex flex-wrap sm:justify-between md:flex-1">
-            {FOOTER_LINKS.map((columns)=>(
-              <FooterColumn></FooterColumn>
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
             ))}
           </div>
         </div>
       </div>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
 
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
 
-type FooterColumnProps={
-  title:string;
-  children:string;
-}
-
-const FooterColumn = ({title, children}:FooterColumnProps)=>{
-<div className="">
-  <h4 className="">{title}</h4>
-</div>
-}
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="">
+      <h4 className="">{title}</h4>
+      {children}
+    </div>
+  );
+};
