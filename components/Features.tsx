9572fa99d9e3337ad211e5d1146f5ac3b597c9f5
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 09:23:17 +0100

    Updated

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function Features() {
   return (
     <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container"></div>
+      <div className="max-container padding-container relative w-full justify-end">
+        <div className="">
+        <Image src="/phone.png" alt="phone" width={50} height={50} />
+        </div>
+      </div>
     </section>
   );
 }
