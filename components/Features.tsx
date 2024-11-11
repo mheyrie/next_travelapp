06c4dc43bcd830ea23cb8c2027a5dd18a579903d
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 09:40:06 +0100

    Updated

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,9 +4,16 @@ export default function Features() {
   return (
     <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
       <div className="max-container padding-container relative w-full justify-end">
-        <div className="">
-        <Image src="/phone.png" alt="phone" width={440} height={1000} className="feature-phone"/>
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
         </div>
+        <div className="z-20 flex w-full flex-col lg:w-[60%]"></div>
       </div>
     </section>
   );
