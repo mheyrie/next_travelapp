5cc116640dad1ad9d4739f3f4b5e42ccc122dcb6
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 12:04:33 +0100

    Updated

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -5,13 +5,35 @@ export default function GetApp() {
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free Now!</h2>
-          <p className="regular-16 text-gray-10">Available on iOS and Android</p>
-          <Button type="button" title="Downlaod App"
-          icon="/apple.png"
-          variant="btn_white" full/>
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+            Get for free Now!
+          </h2>
+          <p className="regular-16 text-gray-10">
+            Available on iOS and Android
+          </p>
+          <Button
+            type="button"
+            title="Download App"
+            icon="/apple.svg"
+            variant="btn_white"
+            full
+          />
+          <Button
+            type="button"
+            title="Download App"
+            icon="/android.svg"
+            variant="btn_white"
+            full
+          />
+          <Button
+            type="button"
+            title="Downlaod App"
+            icon="/apple.svg"
+            variant="btn_white"
+            full
+          />
         </div>
       </div>
     </section>
-  )
+  );
 }
