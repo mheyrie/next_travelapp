cec974e180acdb094d5dae99977d17e74218c2a2
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 09:20:59 +0100

    Updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -71,7 +71,7 @@ export const Hero = () => {
           />
         </div>
       </div>
-      <div className="relative flex flex-1 items-start">
+      <div className="relative flex flex-1 items-center">
         <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
