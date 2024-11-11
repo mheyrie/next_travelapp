1fc3e204446a7e99baf03b274b3a0cd4abddda97
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 13:18:45 +0100

    Updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
   return (
     <footer className="flexCenter mb-24">
-      <div className="padding-container max-container flex w-full flex-col gap-14"></div>
+      <div className="padding-container max-container flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href='/' className="mb-10">
+          <Image
+          className=""
+          src="hilink-logo.svg"
+          alt="boat"
+          width={74}
+          height={29}
+        />
+          </Link>
+          <div className="flex flex-wrap sm:justify-between md:flex-1"></div>
+        </div>
+      </div>
     </footer>
   )
 }
