020687d62e59bd52453b7b93bc0aec6782ed4ebb
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 13:14:31 +0100

    Updating Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="flexCenter mb-24">
+      <div className="padding-container max-container flex w-full flex-col gap-14"></div>
+    </footer>
   )
 }
 export default Footer
