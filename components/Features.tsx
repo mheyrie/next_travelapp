821d8184fc81070b30a8a50b98ee517edbcdf7fc
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 04:35:48 +0100

    Updated

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,5 +1,5 @@
 export default function Features() {
   return (
-    <section className="">Features</section>
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">Features</section>
   )
 }
