c4777fbdef5f15f5b27f235c331e6cfbead0e05e
Mheyrie <omota4fidelity@gmail.com>
2024-11-11 04:33:00 +0100

    Updated

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Guide() {
   return (
     <section className="flexCenter flex-col">
-      <div className="border-1 border-red-600 padding-container max-container w-full pb-24">
+      <div className="padding-container max-container w-full pb-24">
         <Image src="/camp.svg" alt="camp" width={50} height={50} />
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
           Lorem ipsum dolor sit.
